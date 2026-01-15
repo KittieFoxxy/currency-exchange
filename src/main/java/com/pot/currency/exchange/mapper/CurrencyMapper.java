5a7d149c68742f043ac5dba96b7cb590e0f166c7
cat <catlisichkina@gmail.com>
2026-01-15 23:29:27 +0300

    Fix mapper

--- src/main/java/com/pot/currency/exchange/mapper/CurrencyMapper.java
+++ src/main/java/com/pot/currency/exchange/mapper/CurrencyMapper.java
@@ -3,6 +3,7 @@ package com.pot.currency.exchange.mapper;
 import com.pot.currency.exchange.entity.Currency;
 import com.pot.currency.exchange.response.CurrencyResponse;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.factory.Mappers;
 
 import java.util.List;
@@ -12,8 +13,10 @@ public interface CurrencyMapper {
 
     CurrencyMapper INSTANCE = Mappers.getMapper(CurrencyMapper.class);
 
+    @Mapping(target = "name", source = "fullName")
     CurrencyResponse toResponse(Currency currency);
 
+    @Mapping(target = "name", source = "fullName")
     List<CurrencyResponse> toResponseList(List<Currency> currency);
 
 }
